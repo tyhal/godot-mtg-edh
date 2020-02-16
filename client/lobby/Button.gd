7dc47f5138c382895e431c5f27c97323186ed7a7
Tyler Hale <t.w.hale32@gmail.com>
2020-02-16 18:02:12 +1300

    Connect and Disc

--- client/lobby/Button.gd
+++ client/lobby/Button.gd
@@ -1,16 +1,22 @@
 extends Button
 
 const PORT = 5001
+var client = WebSocketClient.new()
 
 func _on_Button_button_down():
-	var client = WebSocketClient.new();
-	var url = "ws://localhost:" + str(PORT) 
+	get_tree().connect("network_peer_connected", self, "_connected")
+	var url = "ws://127.0.0.1:" + str(PORT) 
 	var error = client.connect_to_url(url, PoolStringArray(), true);
-	
-	if error == 0:
-		if client.get_connection_status() == client.CONNECTION_CONNECTED:
-			get_tree().change_scene("res://lobby/Lobby.tscn")
-		else:
-			print("NOT CONNECTED")
-	else:
-		print(error)
+	get_tree().set_network_peer(client);
+	print(error)
+
+func _connected(id):
+	var stat = client.get_connection_status()
+	if stat == client.CONNECTION_CONNECTED:
+		print("CONNECTION_CONNECTED")
+		get_tree().change_scene("res://lobby/Lobby.tscn")
+	if stat == client.CONNECTION_CONNECTING:
+		print("CONNECTION_CONNECTING")
+	if stat == client.CONNECTION_DISCONNECTED:
+		print("CONNECTION_DISCONNECTED")
+
--- server/lobby/Server.gd
+++ server/lobby/Server.gd
@@ -1,7 +1,7 @@
 extends Node
 
 const PORT = 5001
-const MAX_PLAYERS = 2
+const MAX_PLAYERS = 5
 
 var server;
 
@@ -11,11 +11,10 @@ func _ready():
 	server = WebSocketServer.new();
 	server.listen(PORT, PoolStringArray(), true);
 	get_tree().set_network_peer(server)
-	get_tree().connect("network_peer_connected", self, "_client_connected")
-	get_tree().connect("network_peer_disconnected", self, "_client_disconnected")
+	print(get_tree().connect("network_peer_connected", self, "_client_connected"))
+	print(get_tree().connect("network_peer_disconnected", self, "_client_disconnected"))
 	
 	
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if server.is_listening():
@@ -23,11 +22,12 @@ func _process(delta):
 
 func _client_connected(id):
 	print('Client ' + str(id) + ' connected to Server')
-	var newClient = load("res://obj/Card.tscn").instance()
-	newClient.set_position(Vector2(100,100))
+	var newClient = load("res://meta/Client.tscn").instance()
 	newClient.set_name(str(id))     # spawn players with their respective names
 	get_tree().get_root().add_child(newClient)
+	rpc("hello")
 
 func _client_disconnected(id):
-	print("Client " + id + " disconnected from server")
-	get_tree().get_root().remove_child(id)
+	print("Client " + str(id) + " disconnected from server")
+	var r = get_tree().get_root()
+	r.remove_child(r.get_node(str(id)))
