77f8f7c1d59b3d5a251aba6ce70a0fd6b7713d28
Tyler Hale <t.w.hale32@gmail.com>
2020-02-16 17:16:41 +1300

    Server obj

--- client/lobby/Button.gd
+++ client/lobby/Button.gd
@@ -1,14 +1,16 @@
 extends Button
 
-const PORT = 5000
+const PORT = 5001
 
 func _on_Button_button_down():
 	var client = WebSocketClient.new();
-	var url = "ws://127.0.0.1:" + str(PORT) # You use "ws://" at the beginning of the address for WebSocket connections
+	var url = "ws://localhost:" + str(PORT) 
 	var error = client.connect_to_url(url, PoolStringArray(), true);
+	
 	if error == 0:
-		print(error)
-		print(client.get_connection_status())
-		#get_tree().change_scene("res://meta/Table.tscn")
+		if client.get_connection_status() == client.CONNECTION_CONNECTED:
+			get_tree().change_scene("res://lobby/Lobby.tscn")
+		else:
+			print("NOT CONNECTED")
 	else:
 		print(error)
--- server/Server.gd
+++ server/Server.gd
@@ -1,18 +1,20 @@
 extends Node
 
-const PORT = 5000
+const PORT = 5001
 const MAX_PLAYERS = 2
 
 var server;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	print("Setup")
 	server = WebSocketServer.new();
 	server.listen(PORT, PoolStringArray(), true);
 	get_tree().set_network_peer(server)
 	get_tree().connect("network_peer_connected", self, "_client_connected")
 	get_tree().connect("network_peer_disconnected", self, "_client_disconnected")
-
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -21,7 +23,7 @@ func _process(delta):
 
 func _client_connected(id):
 	print('Client ' + str(id) + ' connected to Server')
-	var newClient = load("res://remote_client.tscn").instance()
+	var newClient = load("res://obj/Card.tscn").instance()
 	newClient.set_position(Vector2(100,100))
 	newClient.set_name(str(id))     # spawn players with their respective names
 	get_tree().get_root().add_child(newClient)
