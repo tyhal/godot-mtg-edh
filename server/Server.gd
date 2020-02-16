1b97acebef3ff2e89ace522fb0ab263551ae3af8
largercok <george.mprice44@gmail.com>
2020-02-16 18:17:42 +1300

    client updates 2

--- server/Server.gd
+++ server/Server.gd
@@ -4,7 +4,7 @@ const PORT = 5001
 const MAX_PLAYERS = 2
 
 var server;
-
+var cards = {}
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Setup")
@@ -27,7 +27,9 @@ func _client_connected(id):
 	newClient.set_position(Vector2(100,100))
 	newClient.set_name(str(id))     # spawn players with their respective names
 	get_tree().get_root().add_child(newClient)
+	# Create a new card on the table	
 
 func _client_disconnected(id):
 	print("Client " + id + " disconnected from server")
+	# Remove the card from the table
 	get_tree().get_root().remove_child(id)
